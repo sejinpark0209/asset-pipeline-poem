e23045983fd3ced5adca4ab3876dc5a3e94f5646
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-26 08:50:16 -0700

    pipeline poem bonus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require bootstrap.min.js
+//= require title
+//= require roses_are_red
+//= require violets_are_blue
+//= require compile_your_assets
+//= require then_hit_opt_cmd_u
+//= require poem_meta_data
